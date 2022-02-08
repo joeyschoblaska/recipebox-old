ebc98b1bdac9ce6e3ce296083db9f730d415afc2
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-02-08 16:03:40 -0600

    jest testing

--- src/lib/parseRecipes.test.ts
+++
@@ -1,9 +0,0 @@
-const parseRecipes = require("./parseRecipes");
-
-it("parses a minimal example", () => {
-  const recipes = parseRecipes("# Title\nbody");
-
-  expect(recipes.length).to.equal(1);
-  expect(recipes[0].title).to.equal("Title");
-  expect(recipes[0].body).to.equal("body");
-});
