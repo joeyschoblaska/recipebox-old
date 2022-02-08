9bcc2d924ecdbd65fc4db2a3c6c421183606a81b
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-02-08 15:00:23 -0600

    placeholder in separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
 import React, { useState } from "react";
 import "./index.css";
-
-const placeholder = `# Yorkshire Pudding
-A lovely Yorkie pudd that me nan used to make.
-
-## 1
-* 1/2 pt milk
-* 2 oz butter
-* 5 oz self-raising flour
-
-Heat the milk and butter until nearly boiling. Add flour and allow to seeth over.
-
-## 2
-* 3 eggs
-* salt and pepper to taste
-
-Add the reaming eggs and whisk again. Cook at 220C for about 1 hour.
-`;
+import placeholder from "./lib/placeholder";
 
 const App = () => {
   const [text, setText] = useState(placeholder);
---
+++ src/lib/placeholder.ts
@@ -0,0 +1,18 @@
+const placeholder = `# Yorkshire Pudding
+A lovely Yorkie pudd that me nan used to make.
+
+## 1
+* 1/2 pt milk
+* 2 oz butter
+* 5 oz self-raising flour
+
+Heat the milk and butter until nearly boiling. Add flour and allow to seeth over.
+
+## 2
+* 3 eggs
+* salt and pepper to taste
+
+Add the reaming eggs and whisk again. Cook at 220C for about 1 hour.
+`;
+
+export default placeholder;
