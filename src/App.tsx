591b241c44bf551189e7f005b45430a18225c1c8
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-02-08 11:03:37 -0600

    basic two-col layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-interface AppProps {}
+const App = () => {
+  const [text, setText] = useState("");
 
-const App = ({}: AppProps) => <div className="text-red-200">Hello</div>;
+  return (
+    <div className="grid grid-cols-2 gap-2 h-full">
+      <div className="bg-gray-200 h-screen p-2">
+        <textarea
+          className="resize-none w-full h-full p-2"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="p-4">{text}</div>
+    </div>
+  );
+};
 
 export default App;
