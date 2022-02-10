b8d05a6d313140613e83c8a48eb61189210436bf
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-02-09 23:09:22 -0600

    about

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -43,8 +43,8 @@ const Nav = () => {
           >
             cuisine package
           </a>{" "}
-          for LaTeX, which groups instructions and ingredients together into
-          steps.
+          for LaTeX, which groups instructions and ingredients together under
+          each step.
         </p>
         <ul className="list-inside list-disc pt-3">
           <li>
